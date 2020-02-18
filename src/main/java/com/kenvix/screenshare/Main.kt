f3b0f3ac35a406c3bbf96143bb5d142af20f507f
Kenvix <kenvixzure@live.com>
2020-02-18 21:52:10 +0800

    GUI

--- src/main/java/com/kenvix/screenshare/Main.kt
+++ src/main/java/com/kenvix/screenshare/Main.kt
@@ -5,21 +5,130 @@ package com.kenvix.screenshare
 import com.kenvix.screenshare.network.MulticastServer
 import com.kenvix.screenshare.screen.DefaultFragmentImageProcessor
 import com.kenvix.screenshare.screen.RobotScreenCapturer
-import kotlinx.coroutines.*
+import com.kenvix.screenshare.ui.ClientUI
+import org.apache.commons.cli.*
+import java.net.InetAddress
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.logging.*
+import java.util.logging.Formatter
+import kotlin.system.exitProcess
 
 object Main {
+    const val header = "Kenvix LAN UDP Multicast ScreenShare"
+
+    var options = Options()
+        private set
+    var commands: CommandLine? = null
+        private set
+
     @JvmStatic
-    fun main(args: Array<String>) {
-        val server = MulticastServer()
+    fun main(args: Array<String>?) {
+        commands = getCmd(args)
+
+        if (commands?.hasOption('h') == true) {
+            printHelp()
+            exitProcess(0)
+        }
+
+        println(header)
+        val host = getOptionValue('a', "230.114.5.14")
+        val port = getOptionValue('p', 1919)
+
+        if (commands?.hasOption('s') == true) {
+            runAsServer(host, port)
+        } else {
+            println("Client mode. Target multicast Address: $host    Port: $port")
+        }
+
+        runAsClient()
+
+        println()
+
+        readConsoleCommand()
+    }
+
+    private fun runAsServer(host: String, port: Int) {
+        val server = MulticastServer(multicastAddress = InetAddress.getByName(host), multicastPort = port)
+
         server.onReceive = {
             val data = it.data
-
         }
         server.listen()
 
-        val processor = DefaultFragmentImageProcessor(server)
-        val capturer = RobotScreenCapturer(processor)
+        val processor = DefaultFragmentImageProcessor(server, packetSize = getOptionValue('t', 1000))
+        val capturer = RobotScreenCapturer(processor, fps = getOptionValue('f', 5), monitor = getOptionValue('m', 0))
         capturer.start()
-        Thread.sleep(999999)
+
+        println("Server started at Multicast Address: $host    Port: $port")
+    }
+
+    private fun runAsClient() {
+        val width = getOptionValue('w', 1366)
+        val height = getOptionValue('e', 768)
+
+        ClientUI.getInstance().show(width, height)
+        ClientUI.getInstance().setTitle("Client")
+    }
+
+    private fun readConsoleCommand() {
+        println("Ready to receive command. Please input 'help' for help")
+        val scanner = Scanner(System.`in`)
+
+        while (true) {
+            print("> ")
+            val commandString = scanner.nextLine()
+            val command = commandString.split(' ')
+
+            if (command.isNotEmpty()) {
+                if (command[0] == "help") {
+                    println("You can enter long command below to change options.")
+                    printHelp()
+                } else {
+                    //TODO: Handle command
+                }
+            }
+        }
+    }
+
+    @Throws(ParseException::class)
+    private fun getCmd(args: Array<String>?): CommandLine? {
+        options.addOption("p", "port", true, "Port of screen share render. Default 1919")
+        options.addOption("a", "address", true, "Multicast address of screen share render. Default 230.114.5.14")
+
+        options.addOption("v", "verbose", false, "Verbose logging mode.")
+        options.addOption("s", "server",false, "Render server mode.")
+
+        options.addOption("f", "fps",true, "[Server] FPS. Default 5")
+        options.addOption("m", "monitor",true, "[Server] Monitor ID. Default 0")
+        options.addOption("t", "packet-size",true, "[Server] UDP Packet size. Default 1000")
+        options.addOption("n", "no-loopback",false, "[Server] Do NOT show local playback")
+
+        options.addOption("w", "width",true, "Playback window width")
+        options.addOption("e", "height",true, "Playback window height")
+
+        options.addOption("h", "help", false, "Print help messages")
+
+        val parser = DefaultParser()
+        return parser.parse(options, args)
+    }
+
+    private fun printHelp() {
+        val formatter = HelpFormatter()
+        formatter.printHelp("java -jar ScreenShare.jar", header, options, "", true)
+    }
+
+    inline fun <reified T> getOptionValue(opt: Char, defaultValue: T): T {
+        if (commands == null)
+            return defaultValue
+
+        return when(T::class) {
+            Int::class -> commands!!.getOptionValue(opt, defaultValue.toString()).toInt() as T
+            Float::class -> commands!!.getOptionValue(opt, defaultValue.toString()).toFloat() as T
+            Double::class -> commands!!.getOptionValue(opt, defaultValue.toString()).toDouble() as T
+            Long::class -> commands!!.getOptionValue(opt, defaultValue.toString()).toLong() as T
+            String::class -> commands!!.getOptionValue(opt, defaultValue.toString()) as T
+            else -> throw IllegalArgumentException("Not supported type")
+        }
     }
 }
--- src/main/java/com/kenvix/screenshare/ui/ClientUI.java
+++ src/main/java/com/kenvix/screenshare/ui/ClientUI.java
@@ -6,5 +6,50 @@
 
 package com.kenvix.screenshare.ui;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class ClientUI {
+    private static final ClientUI INSTANCE = new ClientUI();
+    private JFrame frame;
+
+    public static ClientUI getInstance() {
+        return INSTANCE;
+    }
+
+    public void show(int width, int height) {
+        frame = new JFrame();
+        ImagePanel panel = new ImagePanel();
+        setSize(width, height);
+
+        frame.setResizable(true);
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void update() {
+
+    }
+
+    public static BufferedImage resize(BufferedImage img, int newW, int newH) {
+        Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+        BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g2d = dimg.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+
+        return dimg;
+    }
+
+    public void setTitle(String title) {
+        frame.setTitle(title);
+    }
+
+    public void setSize(int width, int height) {
+        Dimension dimension = new Dimension(width, height);
+        frame.setPreferredSize(dimension);
+    }
 }
